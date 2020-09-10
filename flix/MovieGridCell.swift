c51d61a6ac058c4c8a0deb178776f6653b6efa9f
gstawde <gargi.tawde01@gmail.com>
2020-09-09 21:58:23 -0700

    Added new tab for superhero movies. Added functionality that allows for expanded view of movie poster, art, and synopsis.

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Gargi Tawde on 9/9/20.
+//  Copyright © 2020 iamgargious. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
